a5f8d4f2933a8a44a5675ca0dcc3d8e35bb0d929
claudia <claudiachristensen1@gmail.com>
2022-06-05 11:43:58 +0200

    Modificaciones pantalla final

--- js/player.js
+++ js/player.js
@@ -54,9 +54,6 @@ let seleccion = (jugador) => {//jugador nro de 1 a 4
   let imagen1 = document.getElementById("imagenJugador1");
   let imagen2 = document.getElementById("imagenJugador2");
   
-  let nivelVida1 = document.getElementById("stats1");
-  let nivelVida2 = document.getElementById("stats2");
-  
   let cantidadVida1 = document.getElementById("vida1");
   let cantidadVida2 = document.getElementById("vida2");
 
@@ -65,9 +62,6 @@ let seleccion = (jugador) => {//jugador nro de 1 a 4
   const pantallaJuego = () => {
     imagen1.innerHTML = `<img class="fighter1" src="img/${grupo1.nombre}.png" alt="fighter1" >`;
     imagen2.innerHTML = `<img class="fighter2" src="img/${grupo2.nombre}.png" alt="fighter2" >`;
-  
-    nivelVida1.innerHTML = `<div class="lifeBar"><div id="lifeBar1"></div>`;
-    nivelVida2.innerHTML = `<div class="lifeBar"><div id="lifeBar2"></div>`;
 
   };
   
@@ -79,18 +73,14 @@ let seleccion = (jugador) => {//jugador nro de 1 a 4
   
     if (grupo1.vida <= 0) {
       screenSwitch(4);
-  
-      winner.innerHTML = `<img class="winner" src="img/final.jpg" alt="winner" > </br> The winner is Player 2 with ${grupo2.nombre}!`;
+      
+      winner.innerHTML = `<img class="winner" src="img/${grupo2.nombre}.png" alt ="winner"> </br> The winner is Player 2 with ${grupo2.nombre}!`;
     }
     if (grupo2.vida <= 0) {
         screenSwitch(4);
-  
-      winner.innerHTML = `<img class="winner" src="img/final.jpg" alt="winner" > </br> The winner is Player 1 with ${grupo1.nombre}!`;
+   
+      winner.innerHTML = `<img class="winner" src="img/${grupo1.nombre}.png" alt="winner" > </br> The winner is Player 1 with ${grupo1.nombre}!`;
     }
     cantidadVida1.innerHTML = `${grupo1.vida}`;
     cantidadVida2.innerHTML = `${grupo2.vida}`;
-    nivelVida1.innerHTML = `<div class="lifeBar"><div id="lifeBar1"></div>`;
-    nivelVida2.innerHTML = `<div class="lifeBar"><div id="lifeBar2"></div>`;
-    document.getElementById("lifeBar1").style.width = `${grupo1.vida}` + "%";
-    document.getElementById("lifeBar2").style.width = `${grupo2.vida}` + "%";
   };
